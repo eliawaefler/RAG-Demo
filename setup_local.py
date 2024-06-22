6bf7f4e202ca13b966f451d96bef0cd1df1f814a
Elia Wäfler <158746986+eliawaefler@users.noreply.github.com>
2024-06-22 17:22:01 +0200

    Update setup_local.py

--- setup_local.py
+++ setup_local.py
@@ -7,3 +7,15 @@ def download_CLIP():
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", cache_dir="./model_cache")
 
 
+def download_BLIP():
+    from transformers import BlipProcessor, BlipForConditionalGeneration
+
+    # Specify the local directory where you want to save the model and processor
+    local_model_dir = "./model_cache"
+    
+    # Download and save the processor and model locally
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    processor.save_pretrained(local_model_dir)
+    
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+    model.save_pretrained(local_model_dir)
